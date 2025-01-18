19b36800f4ed98a251c9f9ed1501c25cd61f8404
Yo-quiz <cebolleto0@gmail.com>
2025-01-18 22:01:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const yoKaiList = [
   { "name": "Duque Lametodo", "img": "Duque_Lametodo.png" },
   { "name": "Chupatricio", "img": "Chupatricio.png" },
   { "name": "Zazel", "img": "Zazel.png" },
-  { "name": "Zazerilio", "img": "Zazerilio.png" },
+  { "name": "Zazerilio", "img": "Zazelirio.png" },
   { "name": "Zarpadorada", "img": "Zarpadorada.png" },
   { "name": "Vueltodorado", "img": "Vueltodorado.png" },
   { "name": "Intelecto Iluminado", "img": "Intelecto_Iluminado.png" },
